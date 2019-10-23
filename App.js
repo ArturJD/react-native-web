23d651a0e569961e0d1e40c85a064234a5af43e8
Artur <artur@jdsystem.com.br>
2019-10-23 17:04:21 -0300

    .

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { Spring, config } from 'react-spring/renderprops'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Um app react native na interwebs 😎</Text>
-      <Button title='Teste' onPress={()=>alert('tudo teste')}/>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      flag: false
+    }
+  }
+
+  _toggleFlag(){
+    this.setState({
+      flag: !this.state.flag
+    })
+  }
+
+  render() {
+    return (
+      <Spring 
+        //showAndHide={{opacity: 1}, {opacity: 0}}
+        from={{ opacity: 0, marginLeft: -60 }} 
+        to={{ opacity: 1, marginLeft: 0 }} 
+        config={config.slow}
+        >
+        {props=>(
+          <View style={styles.container}>
+            <Text style={props}>Um app react native na interwebs 😎</Text>
+            <Button title='Teste' onPress={() => { alert('Só funfa o alert padrãozinho'); this._toggleFlag() }} />
+          </View>
+        )}
+      </Spring>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Expo Native Web
 
 > https://medium.com/@toastui/from-zero-to-publish-expo-web-react-native-for-web-tutorial-e3e020d6d3ff
+> https://www.react-spring.io/docs/props/spring
 
 
 Em teste
