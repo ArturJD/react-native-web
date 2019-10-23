81e6e7dd92c234b682c5b465ee40d628034dbc35
Artur <artur@jdsystem.com.br>
2019-10-23 13:59:10 -0300

    Inicial

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Um app react native na interwebs 😎</Text>
+      <Button title='Teste' onPress={()=>alert('tudo teste')}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# Expo Native Web
+
+> https://medium.com/@toastui/from-zero-to-publish-expo-web-react-native-for-web-tutorial-e3e020d6d3ff
+
+
+Em teste
+
